82d8924a138e075e94f9f08f70f320e29d5b58cd
Jason Douglas <jdouglas71@gmail.com>
2015-03-21 17:00:43 -0700

    Buncha of tests

--- js/touchjet.js
+++ js/touchjet.js
@@ -15,29 +15,42 @@ $(document).ready(function()  {
 	// Cache the Window object
 	$window = $(window);
 
-	/**
+    /** Background Section Processing */
+    //The panel sizes 
+    //JGD TODO: Read these on load.
+    //var panelSizes = new Array( 1475, 728 );//, 1200, 1200, 1062, 1228 );
+    //Calculate the size of the section based on all the panels and set it. 
+    //var sectionSize = 0, i = 0;
+    //for(i=0; i < panelSizes.length; i++)
+    //{   
+    //    sectionSize += Math.round(panelSizes[i]/2);
+    //}
+    //$('#bg-panel').height( sectionSize );
+    //var coords = getCoordStr( 0 );
+    //$('#bg-panel').css({ backgroundPosition: coords });
+	
+    /**
 	 * Move the background sections.
 	 */
 	$('section[data-type="background"]').each(function(){
 		var $bgobj = $(this); // assigning the object
                     
 		$(window).scroll(function() {
-                    
 			// Scroll the background at var speed
 			// the yPos is a negative value because we're scrolling it UP!								
-			var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-			var yBGMax = '-1245';
-			console.log( "BG: " + yPos );
-			if( yPos < yBGMax )  yPos = yBGMax;    
-			// Put together our final background position
-			var coords = '50% '+ yPos + 'px';
-	
+			var yPos = -($window.scrollTop());/// $bgobj.data('speed'));
+            var coords = '50% ' + yPos + 'px';
+            //var coords = getCoordStr( yPos );
 			// Move the background
 			$bgobj.css({ backgroundPosition: coords });
-		
 		}); // window scroll Ends
 	});
 
+    /** Foreground Section Processing */
+    var topLimit = 300; //As measured from top
+    //var bottomLimit = 300; //As measured from bottom
+    //var foregroundLimit = sectionSize - topLimit - bottomLimit;
+    //var bgFGRatio = foregroundLimit / sectionSize;
 	/**
 	 * Move the foreground section.
 	 */
@@ -45,24 +58,31 @@ $(document).ready(function()  {
 		var $fgobj = $(this); // assigning the object
                     
 		$(window).scroll(function() {
-			var yPos = (($window.scrollTop()+300) / $fgobj.data('speed')); 
-			console.log( "FG: " + yPos );
-			//if( yPos < 300 ) yPos = 300;
-			var yFGMax = '497';
-			//Don't go past the max.
-			if( yPos > yFGMax ) yPos = yFGMax;
-			// Put together our final background position
+			var yPos = (($window.scrollTop()+topLimit));
+            // Put together our final background position
 			var coords = '68% '+ yPos + 'px';
-	
 			// Move the background
 			$fgobj.css({ backgroundPosition: coords });
 		}); // window scroll Ends
-
 	});	
+
+    /**
+     * Calculate the coords string based on the yPos
+     */
+    function getCoordStr(yPos)
+    {
+		// Put together our final background position
+		var coords = '50% '+ yPos + 'px,';
+        for(i=0; i<(panelSizes.length - 1);i++)
+        {
+            coords += '50% ' + (yPos+(Math.round(panelSizes[i]/2))) + 'px,';
+        }
+        console.log( "coords: " + coords );
+        return coords;
+    }
 }); 
 
-
-/* 
+/** 
  * Create HTML5 elements for IE's sake
  */
 document.createElement("article");
